1b38de117251e5f2d4b3f045146692877d5fd045
Nickan <nikanakbari83@gmail.com>
2026-08-27 07:16:14 +0330

    Add random case selection

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import CasePlayer from "../components/CasePlayer";
 import { cases } from "../data/cases";
 
 export default function Home() {
-  const caseData = cases[0];
+  const [caseData, setCaseData] = useState(cases[0]);
 
-  return (
-    <CasePlayer
-      caseData={caseData}
-    />
-  );
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * cases.length);
+    setCaseData(cases[randomIndex]);
+  }, []);
+
+  return <CasePlayer caseData={caseData} />;
 }
